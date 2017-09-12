d1621ce51c7a1c50a761aca0b83dcbc94fe0f8e2
haykatoyan <hayk.atoyan@yahoo.com>
2017-09-12 18:39:58 +0400

    first commit

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,82 +1,87 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://admin:admin@ds133104.mlab.com:33104/mytasklist_brad', ['tasks']);
+var db = mongojs('mongodb://<dbuser>:<dbpassword>@ds149069.mlab.com:49069/mytasklist_as', ['tasks']);
 
-//get all tasks
-router.get('/tasks', function(req, res, next) {
-	db.tasks.find(function(err, tasks) {
-		if(err) {
-			res.send(err);
-		}
-		res.json(tasks);
-	});
+// Get All Tasks
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(tasks);
+        }
+    });
 });
 
-//get single task
-router.get('/tasks/:id', function(req, res, next) {
-	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
-		if(err) {
-			res.send(err);
-		}
-		res.json(task);
-	});
+// Get Single Task
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(task);
+        }
+    });
 });
 
-//save task
-router.post('/tasks', function(req, res, next) {
-	var task = req.body;
-
-	if(!task.title || (task.isDone + '')) {
-		res.status(400);
-		res.json({
-			error: 'Bad data'
-		});
-	} else {
-		db.tasks.save(task, function(err, task) {
-			if(err) {
-				res.send(err);
-			}
-			res.json(task);
-		});
-	}
-
+// Save Task
+router.post('/task', function(req, res, next){
+    var task = req.body;
+    if(!task.title || (task.isDone + '')){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    }else {
+        db.tasks.save(task, function(err, task){
+            if(err){
+                res.send(err);
+            }else {
+                res.json(task);
+            }
+        });
+    }
 });
 
-//delete task
-router.delete('/tasks/:id', function(req, res, next) {
-	db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
-		if(err) {
-			res.send(err);
-		}
-		res.json(task);
-	});
+// Delete Task
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(task);
+        }
+    });
 });
 
-//update task
-router.put('/tasks/:id', function(req, res, next) {
-	var task = req.body;
-	var updTask = {};
+// Update Task
+router.put('/task/:id', function(req, res, next){
+    var task = req.body;
+    var updTask = {};
 
-	if(task.isDone)
-		updTask.isDone = task.isDone;
+    if(task.isDone){
+        updTask.isDone = task.isDone;
+    }
 
-	if(task.title)
-		updTask.isDone = task.title;
+    if(task.title){
+        updTask.title = task.title;
+    }
 
-	if(!updTask) {
-		res.status(400);
-		res.json({
-			error: 'Bad data'
-		});
-	} else {
-		db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, updTask, {}, function(err, task) {
-			if(err) {
-				res.send(err);
-			}
-			res.json(task);
-		});
-	}
+    if(!updTask){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    }else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(task);
+        }
+    });
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,23 +9,21 @@ var port = 3000;
 
 var app = express();
 
-//view engine
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-//static folder
+// Set Static folder
 app.use(express.static(path.join(__dirname, 'client')));
 
-//body parser middleware
+// Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
 app.use('/api', tasks);
 
-app.listen(port, function() {
-	console.log('Server started on port 3000');
+app.listen(port, function(){
+    console.log('Server started on port ' + port);
 });
-
-
